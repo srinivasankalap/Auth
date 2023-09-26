49e6a689618c49e4f0f7199c87d5a5e6ccbc6236
srinivasankalap <cnu.ankalap@gmail.com>
2023-09-26 17:15:00 +0530

    LocalStorage for login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authCtx=useContext(AuthContext);
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!authCtx.isLoggedIn && (<Route path='/auth'>
           <AuthPage />
-        </Route>
-        <Route path='/profile'>
+        </Route>)}
+        {authCtx.isLoggedIn && (<Route path='/profile'>
           <UserProfile />
-        </Route>
+        </Route>)}
+      <Route path='*'>
+        <Redirect to='/' />
+      </Route>
       </Switch>
     </Layout>
   );
